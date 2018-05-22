7ad20033f32770e0455266de0470da85f42b5b10
nico <529725067@qq.com>
2018-05-22 18:35:33 +0800

    Growing.h

--- PublicHeader/Growing.h
+++ PublicHeader/Growing.h
@@ -6,12 +6,13 @@
 //  Copyright © 2018年 GrowingIO. All rights reserved.
 //
 
-#import <GrowingCoreKit/GrowingCoreKit.h>
-
 #if !defined(__has_include)
 #error "Growing.h won't import anything if your compiler doesn't support __has_include. Please \
 import the headers individually."
 #else
+    #if __has_include(<GrowingCoreKit/GrowingCoreKit.h>)
+    #import <GrowingCoreKit/GrowingCoreKit.h>
+    #endif
     #if __has_include(<GrowingAutoTrackKit/GrowingAutoTrackKit.h>)
     #import <GrowingAutoTrackKit/GrowingAutoTrackKit.h>
     #endif
